f91a975e3eaa987e8b927b5e16d021c257d4ad88
Taiwo AKerele <davcodes24@gmail.com>
2023-11-15 14:58:18 +0100

    fix bug

--- app/Candidate/ProfileDetails.jsx
+++ app/Candidate/ProfileDetails.jsx
@@ -1,27 +1,52 @@
+import { useEffect } from "react";
+import UserForm from "./UserForm";
 const ProfileDetails = () => {
-    return (
-
-        
-        <div className="form-container">
+  return (
+    
+    useEffect(() => {
+        if (typeof localStorage !== 'undefined') {
+            return <div className="form-container">
             <form className="form">
-                <label htmlFor="user-company-name">Company Name:</label>
-                <input type="text" value={localStorage.getItem('Cname') }  id="user-company-name" />
-                <label htmlFor="email">Email</label>
-                <input type="email" value={localStorage.getItem('Cmail')}  id="email" />
-
-                <label>Mobile:</label>
-                <input type="tel" name="tel" value={localStorage.getItem('Ctelephone')}  id="user-company-telephone" />
-
-                <label>Address</label>
-                <input type="text" name="address" value={localStorage.getItem('Caddress')}  id="user-company address" />
-
-                <label>Motto:</label>
-                <input type="text" name="motto" value={localStorage.getItem('Cmotto')}  id="user-company-motto" />
-
-                <input type="submit" value="Done" className="text-white bg-green-800" />
+              <label htmlFor="user-company-name">Company Name:</label>
+              <input
+                type="text"
+                value={localStorage.getItem("Cname")}
+                id="user-company-name"
+              />
+              <label htmlFor="email">Email</label>
+              <input type="email" value={localStorage.getItem("Cmail")} id="email" />
+      
+              <label>Mobile:</label>
+              <input
+                type="tel"
+                name="tel"
+                value={localStorage.getItem("Ctelephone")}
+                id="user-company-telephone"
+              />
+      
+              <label>Address</label>
+              <input
+                type="text"
+                name="address"
+                value={localStorage.getItem("Caddress")}
+                id="user-company address"
+              />
+      
+              <label>Motto:</label>
+              <input
+                type="text"
+                name="motto"
+                value={localStorage.getItem("Cmotto")}
+                id="user-company-motto"
+              />
+      
+              <input type="submit" value="Done" className="text-white bg-green-800" />
             </form>
-        </div>
-    );
-}
+          </div>
+        }
+        
+      }, [])
+  );
+};
 
 export default ProfileDetails;
