3c4d30ff548ca73ddf8bca34c8e34ad05387e3c9
Taiwo AKerele <davcodes24@gmail.com>
2023-11-18 00:54:57 +0100

    working on the /about page

--- app/comps/Header.jsx
+++ app/comps/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
               </Link>
             </span>
             <div className="absolute lister">
-              <Link href="/">
+              <Link href="/about">
                 <List list="About US" />
               </Link>
               <Link href="/Jobs">
